155eb76e1e98cb7e2e96a2214f7cc1683df4829e
Aiswarya Suresh <Aiswarya.Suresh555@gmail.com>
2018-10-30 00:20:50 -0400

    committed

---
+++ BasicMVC/Global.asax.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using MyBlogEngine.Models;
+
+namespace MyMVC
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static BlogArticleRepository mainBlog;
+        // sample rootPath 
+        // private string rootPath = @"C:\Users\user\Downloads\MyMVC\blogFiles";
+        private string rootPath = null;
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //Change it to your folder path
+            rootPath = @"C:\Users\user\Downloads\MyMVC\blogFiles";
+            if (rootPath == null)
+            {
+                throw new Exception("You have to set the rootPath to your blogFiles directory");
+            }
+            mainBlog = new BlogArticleRepository(rootPath, "blog.dat");
+        }
+    }
+}
---
+++ MyBlogEngine/Models/BlogArticleRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace MyBlogEngine.Models
+{
+    public class BlogArticleRepository : List<BlogArticle>
+    {
+        public string RootPath { get; set; }
+        private string blogFile;
+
+        public BlogArticleRepository(string rootPath, string blogFile)
+        {
+            this.RootPath = rootPath;
+            this.blogFile = blogFile;
+            GetAllBlogArticles(false);
+        }
+
+        public void GetAllBlogArticles(bool? forceReload)
+        {
+            if (this.Count == 0 || (forceReload == true))
+            {
+                this.Clear();
+                LoadBlogArticlesFromDataStore();
+            }
+        }
+        
+        private void LoadBlogArticlesFromDataStore()
+        {
+            string[] allLines = File.ReadAllLines(Path.Combine(RootPath,blogFile)); 
+            foreach (string line in allLines)
+            {
+                try
+                {
+                    string[] allItems = line.Split('|');
+
+                    BlogArticle ba = new BlogArticle();
+                    ba.Id = Convert.ToInt32(allItems[0]); 
+                    ba.Title = allItems[1];
+                    ba.Category = allItems[2];
+                    ba.RelativeFilePath = allItems[3];
+                    ba.Created = DateTime.Parse(allItems[4]);
+                    this.Add(ba);
+                }
+                finally
+                {
+
+                }
+            }
+        }
+
+    }
+}
